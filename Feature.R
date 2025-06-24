20146b0589120ef337f8ffa2f69651b5afaab0fd
Amin Douioui <modo00004@uni-saarland.de>
2025-06-24 14:56:09 +0200

    Features.R aktualisiert

--- Feature.R
+++ Feature.R
@@ -2,11 +2,11 @@ library(tidyverse)
 library(janitor)
 library(tidymodels)
 
-
 bdata <- read_delim("data/bank/bank-full.csv", delim = ";")
 
 bdata <- bdata %>%
   mutate(
+    # Originale Faktor-Konvertierungen
     marital = as.factor(marital),
     job = as.factor(job),
     education = as.factor(education),
@@ -16,8 +16,52 @@ bdata <- bdata %>%
     contact = as.factor(contact),
     month = as.factor(month),
     poutcome = as.factor(poutcome),
-    y = as.factor(y)  # Zielvariable auch als Faktor!
-  )
-
-dir.create("data/processed", recursive = TRUE, showWarnings = FALSE)
-saveRDS(bdata, file = "data/processed/bdata.rds")
+    y = as.factor(y),
+    
+    # Neue Feature-Engineering-Schritte
+    # 1. Numerische Kodierungen
+    job_numeric = ifelse(job == "unemployed", 1, 0),
+    marital_numeric = case_when(
+      marital == "single" ~ 0,
+      marital == "married" ~ 1,
+      marital == "divorced" ~ 2,
+      TRUE ~ NA_real_
+    ),
+    education_numeric = case_when(
+      education == "primary" ~ 0,
+      education == "secondary" ~ 1,
+      education == "tertiary" ~ 2,
+      TRUE ~ NA_real_
+    ),
+    default_numeric = ifelse(default == "yes", 1, 0),
+    housing_numeric = ifelse(housing == "yes", 1, 0),
+    loan_numeric = ifelse(loan == "yes", 1, 0),
+    y_numeric = ifelse(y == "yes", 1, 0),
+    
+    # 2. Spezielle Transformationen
+    log_balance = log(abs(balance) + 1),  # log(x+1) Transformation
+    
+    # 3. pdays Behandlung
+    pdays_category = as.factor(ifelse(pdays == -1, "no_previous_contact", "had_contact")),
+    
+    # 4. Zusätzliche sinnvolle Features
+    age_group = cut(age, breaks = c(0, 25, 45, 65, 100),
+                    contact_season = case_when(
+                      month %in% c("mar", "apr", "may") ~ "spring",
+                      month %in% c("jun", "jul", "aug") ~ "summer",
+                      month %in% c("sep", "oct", "nov") ~ "autumn",
+                      TRUE ~ "winter"
+                    ),
+                    balance_sign = sign(balance)
+    ),
+    
+    # Target Encoding für poutcome (separater Schritt wegen Gruppenoperation)
+    poutcome_means <- bdata %>%
+      group_by(poutcome) %>%
+      summarise(poutcome_numeric = mean(y_numeric, na.rm = TRUE)),
+    
+    bdata <- bdata %>%
+      left_join(poutcome_means, by = "poutcome"),
+    
+    dir.create("data/processed", recursive = TRUE, showWarnings = FALSE),
+    saveRDS(bdata, file = "data/processed/bdata.rds")
